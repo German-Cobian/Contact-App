5f34a1d13a917edca81d8d8b5b16276516f754ff
German Renato Cobian <germancobian@hotmail.com>
2022-01-16 12:20:56 -0800

    Implement Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import { ToastContainer } from 'react-toastify'
-import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
       <Navbar />
-      <h2>Welcome to React-Redux Contact Book</h2>
+      <Switch >
+        <Route exact path="/" component={() => <Home/>} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <h1>Welcome to React Redux Contact Book</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
